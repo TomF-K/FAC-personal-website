e19aee722d9523c12f0fdcabbacb4235138174fc
Tom Forman-Kenrick <tom.kenrick@gmail.com>
2023-11-21 21:14:32 +0000

    updated app.js

---
+++ app.js
@@ -0,0 +1,66 @@
+// testimonials data
+
+const students = [
+    {
+        id: 1,
+        author: 'Stew B.',
+        location: 'Somerset, UK',
+        img: 'imgs/SB_profile.jpg',
+        text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+
+    },
+    {
+        id: 2,
+        author: 'Erik P.',
+        location: 'Someplace, MA',
+        img: 'imgs/SB_profile.jpg',
+        text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+
+    },
+    {
+        id: 3,
+        author: 'Alex B.',
+        location: 'Oxford, UK',
+        img: 'imgs/SB_profile.jpg',
+        text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+
+    },
+    {
+        id: 4,
+        author: 'Jim D.',
+        location: 'Somerset, UK',
+        img: 'imgs/SB_profile.jpg',
+        text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+
+    },
+    {
+        id: 5,
+        author: 'Dave D-R.',
+        location: 'Athens, Greece',
+        img: 'imgs/SB_profile.jpg',
+        text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+
+    }
+]
+
+// target elements:
+
+const img = document.getElementById('student-img');
+const author = document.getElementById('testimonials__author');
+const location = document.getElementById('testimonials__location');
+const testimonialText = document.getElementById('testimonials__text');
+
+// location
+// text
+
+// prev button
+// next button
+
+
+// load default testimonial
+
+// load testimonial based on student
+
+// load previous 
+
+// load next
